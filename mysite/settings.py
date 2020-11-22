55b715bca6eaabbaec366647124fffb7be9ef58e
Donghyun1228 <dawnme1228@naver.com>
2020-11-22 15:33:28 +0900

    11.22

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+72m9%8sz2!mbdz9b@hgyio!z%pttfz&!)d)jb0_r6x1#$d95s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1}', '.pythonanywhere.com']
 
 
 # Application definition
