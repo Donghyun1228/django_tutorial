1994c69907d79a6a47b6f6920382fb625e71d6ce
Donghyun1228 <dawnme1228@naver.com>
2020-11-26 00:11:30 +0900

    result view, class view

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from .models import Question, Choice
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
 def index(request):
@@ -38,7 +39,28 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/result.html', {'question': question})
+
+'''class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'''
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -53,3 +75,4 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
