a5b94f0ed8f2c362ac75864960694582ad9deb9d
Donghyun1228 <dawnme1228@naver.com>
2020-11-27 15:44:10 +0900

    test 작성

--- polls/urls.py
+++ polls/urls.py
@@ -4,17 +4,20 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
+# 함수 기반 뷰 url
     # ex: /polls/
-    #path('', views.index, name='index'),
+    path('', views.index, name='index'),
     # ex: /polls/5
-    #path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    #path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    #path('<int:question_id>/vote/', views.vote, name='vote')'''
-
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/result', views.ResultView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote')
 ]
+
+# 클래스 기반 뷰 url
+    #path('', views.IndexView.as_view(), name='index'),
+    #path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    #path('<int:pk>/result', views.ResultView.as_view(), name='results'),
+    #path('<int:question_id>/vote/', views.vote, name='vote')
+
