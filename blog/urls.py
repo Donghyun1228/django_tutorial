8a6d6290cca00e80adb2e27b5be34892fdc6b0ab
Donghyun1228 <dawnme1228@naver.com>
2020-12-07 18:44:22 +0900

    blog url view

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 app_name = 'blog'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    # /blog/
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>', views.update, name='update'),
+    path('<int:pk>', views.delete, name='delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.db import models
 from django.http import HttpResponse, HttpResponseRedirect
+'''from .models import Post'''
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Dooboo blog")
+    latest_post_list = Post.objects.order_by('-date')
+    return render(request, 'blog/index.html', {'latest_post_list': latest_post_list})
+
+def create(request):
+    return HttpResponse('Post Create')
+
+def detail(request):
+    return HttpResponse('Post Detail')
+
+def update(request):
+    return HttpResponse('Post Update')
+
+def delete(request):
+    return HttpResponse('Post Delete')
