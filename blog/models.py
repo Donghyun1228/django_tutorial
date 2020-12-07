d863ae921ae17aa4eb2d807d7ae114055f8de4a9
Donghyun1228 <dawnme1228@naver.com>
2020-12-07 17:41:43 +0900

    config Blog

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    post_title = models.CharField(max_length=200)
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=10)
+    date = models.TimeField(auto_now_add=True)
+    content = models.TextField(default=' ')
+
     def __str__(self):
-        return self.post_title
+        return self.title
 
