2bd1d37dc1036ac473a2ce3c17b3c74c2b9d5ef6
Donghyun1228 <dawnme1228@naver.com>
2020-12-24 00:26:57 +0900

    CRUD views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db import models
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
@@ -11,14 +13,23 @@ def index(request):
     return render(request, 'blog/index.html', {'latest_post_list': latest_post_list})
 
 def create(request):
-    return HttpResponse('Post Create')
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('blog:index'))
 
 def detail(request, post_id):
-    selected_post = Post.objects.get(pk=post_id)
+    selected_post = get_object_or_404(Post, pk=post_id)
     return render(request, 'blog/detail.html', {'selected_post': selected_post})
-    
-def update(request):
+
+def update(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
     return HttpResponse('Post Update')
 
-def delete(request):
-    return HttpResponse('Post Delete')
+def delete(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
+    return HttpResponseRedirect(reverse('blog:index'))
