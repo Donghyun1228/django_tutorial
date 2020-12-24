51459bc3437d6e964dac99027d2b0385a1e59109
Donghyun1228 <dawnme1228@naver.com>
2020-12-24 17:12:43 +0900

    CRUD 기능 구현

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
     path('<int:post_id>/', views.detail, name='detail'),
-    path('<int:post_id>', views.update, name='update'),
-    path('<int:post_id>', views.delete, name='delete'),
+    path('<int:post_id>/update/', views.update, name='update'),
+    path('<int:post_id>/delete/', views.delete, name='delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -19,6 +19,10 @@ def create(request):
             form.save()
             return HttpResponseRedirect(reverse('blog:index'))
 
+    else:
+        form = PostForm()        
+        return render(request, 'blog/create.html', {'form' : form})
+
 def detail(request, post_id):
     selected_post = get_object_or_404(Post, pk=post_id)
     return render(request, 'blog/detail.html', {'selected_post': selected_post})
@@ -27,7 +31,13 @@ def update(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
-    return HttpResponse('Post Update')
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('blog:detail', args=(post.id,)))
+    
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/update.html', {'form' : form})
 
 def delete(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
